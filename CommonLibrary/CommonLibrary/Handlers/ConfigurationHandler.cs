9f4fd4ef21bd9e6511ef21fb36178ef7177d9e40
DomenicoPou <domenicopoutanen@gmail.com>
2020-08-03 20:46:41 +1000

    Added configuration file handling (JSON)
    Day 1: Added basic json serialising/deserializing with newton soft. To then handle Config/ClassName.Json files

---
+++ CommonLibrary/CommonLibrary/Handlers/ConfigurationHandler.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text;
+using System.Windows;
+
+namespace CommonLibrary.Handlers
+{
+    public static class ConfigurationHandler
+    {
+        // T will be the main configuartion class
+        private static string configurationFolderName = "Config";
+
+        private static string GetConfigFile<T>()
+        {
+            // Return the configuration file
+            return $@"{AppDomain.CurrentDomain.BaseDirectory}/{configurationFolderName}/{typeof(T).Name}.json";
+        }
+
+        public static T ReadConfig<T>()
+        {
+            // Get the congfiguration File
+            string configFile = GetConfigFile<T>();
+            
+            // Create the file if it doesn't exist
+            FileHandler.CreateFile(configFile);
+
+            // Deserialize Json to Object and Return It
+            using (var fileStream = new FileStream(configFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var textReader = new StreamReader(fileStream))
+            {
+                string content = textReader.ReadToEnd();
+                return JsonConvert.DeserializeObject<T>(content);
+            }
+        }
+
+        public static void WriteConfig<T>(T configurationObject)
+        {
+            // Grab the configuration file
+            string configFile = GetConfigFile<T>();
+            
+            // Create the file if it doesn't exist
+            FileHandler.CreateFile(configFile);
+
+            // Serialize the object to JSON indented
+            string fileContent = JsonConvert.SerializeObject(configurationObject, Formatting.Indented);
+
+            // Write the string into the config file
+            using (StreamWriter writer = new StreamWriter(configFile, false, Encoding.Unicode))
+            {
+                writer.Write(fileContent);
+            }
+        }
+    }
+}
---
+++ CommonLibrary/CommonLibrary/Handlers/FileHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CommonLibrary.Handlers
+{
+    public static class FileHandler
+    {
+        public static void CreateFile(string fileName)
+        {
+            // Create the Directory of the file if it doesn't exist
+            if (!Directory.Exists(Path.GetDirectoryName(fileName)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(fileName));
+            }
+
+            // Create file if it doesn't exist
+            if (!File.Exists(fileName))
+            {
+                File.Create(fileName);
+            }
+        }
+    }
+}
