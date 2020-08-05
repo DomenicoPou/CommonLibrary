ad38aa34b17b3c6bf23e7484b77a7c366d269f10
DomenicoPou <domenicopoutanen@gmail.com>
2020-08-05 17:12:08 +1000

    Added comments for readability

--- CommonLibrary/CommonLibrary/Handlers/ConfigurationHandler.cs
+++ CommonLibrary/CommonLibrary/Handlers/ConfigurationHandler.cs
@@ -6,31 +6,52 @@ using System.Windows;
 
 namespace CommonLibrary.Handlers
 {
+    /// <summary>
+    /// This configuration handler implements and handles a configuration folder with json files
+    /// </summary>
+    /// <typeparam name="T">The type of configuration being handled</typeparam>
     public static class ConfigurationHandler <T>
     {
         // T will be the main configuartion class
         private static string configurationFolderName = "Config";
 
+        /// <summary>
+        /// Initial constructure  
+        /// </summary>
         static ConfigurationHandler()
         {
             SetupConfiguration();
         }
 
+        /// <summary>
+        /// Writes the base configuration model if the file doesn't exist
+        /// </summary>
         private static void SetupConfiguration()
         {
+            // Get the configuration name
             string configurationName = GetConfigFile();
+            
+            // If the file doesn't exist. Write the default (null if object) to it.
             if (!File.Exists(configurationName))
             {
                 WriteConfig(default(T));
             }
         }
 
+        /// <summary>
+        /// Gets the configuration file full path depending on the configures type
+        /// </summary>
+        /// <returns>{ApplicationLocation}/Config/{TypeName}.json</returns>
         private static string GetConfigFile()
         {
             // Return the configuration file
             return $@"{AppDomain.CurrentDomain.BaseDirectory}/{configurationFolderName}/{typeof(T).Name}.json";
         }
 
+        /// <summary>
+        /// Read the created configuration file.
+        /// </summary>
+        /// <returns>The object of the configuration type</returns>
         public static T ReadConfig()
         {
             // Get the congfiguration File
@@ -45,6 +66,10 @@ namespace CommonLibrary.Handlers
             }
         }
 
+        /// <summary>
+        /// Write to the configuration fil.
+        /// </summary>
+        /// <param name="configurationObject">The configuration object</param>
         public static void WriteConfig(T configurationObject)
         {
             // Grab the configuration file
--- CommonLibrary/CommonLibrary/Handlers/FileHandler.cs
+++ CommonLibrary/CommonLibrary/Handlers/FileHandler.cs
@@ -5,8 +5,15 @@ using System.Text;
 
 namespace CommonLibrary.Handlers
 {
+    /// <summary>
+    /// Misc file handling functions
+    /// </summary>
     public static class FileHandler
     {
+        /// <summary>
+        /// Creates the full file path if it doesn't exist
+        /// </summary>
+        /// <param name="fileName">The file path being created</param>
         public static void CreateFile(string fileName)
         {
             // Create the Directory of the file if it doesn't exist
